f8b6831da6450df21df822fff6b0f2a5693f4504
Lavanya Sharma <Larazinbox@gmail.com>
2025-12-17 00:16:04 -0800

    update hackathons.js file to include state and country tags for events

--- map-app/fixtures/hackathons.js
+++ map-app/fixtures/hackathons.js
@@ -5,6 +5,8 @@ const hackathons =
     "title": "Hack The North",
     "university": "University of Waterloo",
     "city": "Waterloo",
+    "state": "ON",
+    "country": "Canada",
     "coordinates": [43.4723, -80.5449]
   },
   {
@@ -12,6 +14,8 @@ const hackathons =
     "title": "TreeHacks",
     "university": "Stanford University",
     "city": "Stanford",
+    "state": "CA",
+    "country": "USA",
     "coordinates": [37.4275, -122.1697]
   },
   {
@@ -19,6 +23,8 @@ const hackathons =
     "title": "MHacks",
     "university": "University of Michigan",
     "city": "Ann Arbor",
+    "state": "MI",
+    "country": "USA",
     "coordinates": [42.278, -83.7382]
   },
   {
@@ -26,6 +32,8 @@ const hackathons =
     "title": "Cal Hacks",
     "university": "UC Berkeley",
     "city": "Berkeley",
+    "state": "CA",
+    "country": "USA",
     "coordinates": [37.8715, -122.273]
   },
   {
@@ -33,6 +41,8 @@ const hackathons =
     "title": "PennApps",
     "university": "University of Pennsylvania",
     "city": "Philadelphia",
+    "state": "PA",
+    "country": "USA",
     "coordinates": [39.9522, -75.1932]
   },
   {
@@ -40,6 +50,8 @@ const hackathons =
     "title": "HackHarvard",
     "university": "Harvard University",
     "city": "Cambridge",
+    "state": "MA",
+    "country": "USA",
     "coordinates": [42.377, -71.1167]
   },
   {
@@ -47,6 +59,8 @@ const hackathons =
     "title": "HackGT",
     "university": "Georgia Institute of Technology",
     "city": "Atlanta",
+    "state": "GA",
+    "country": "USA",
     "coordinates": [33.7756, -84.3963]
   },
   {
@@ -54,6 +68,8 @@ const hackathons =
     "title": "LA Hacks",
     "university": "UCLA",
     "city": "Los Angeles",
+    "state": "CA",
+    "country": "USA",
     "coordinates": [34.0689, -118.4452]
   },
   {
@@ -61,6 +77,8 @@ const hackathons =
     "title": "HackIllinois",
     "university": "University of Illinois Urbana-Champaign",
     "city": "Urbana-Champaign",
+    "state": "IL",
+    "country": "USA",
     "coordinates": [40.1106, -88.2073]
   },
   {
@@ -68,6 +86,8 @@ const hackathons =
     "title": "HackTX",
     "university": "University of Texas at Austin",
     "city": "Austin",
+    "state": "TX",
+    "country": "USA",
     "coordinates": [30.2849, -97.7341]
   },
   {
@@ -75,6 +95,8 @@ const hackathons =
     "title": "Boilermake",
     "university": "Purdue University",
     "city": "West Lafayette",
+    "state": "IN",
+    "country": "USA",
     "coordinates": [40.4237, -86.9212]
   },
   {
@@ -82,6 +104,8 @@ const hackathons =
     "title": "HackPrinceton",
     "university": "Princeton University",
     "city": "Princeton",
+    "state": "NJ",
+    "country": "USA",
     "coordinates": [40.3431, -74.6551]
   },
   {
@@ -89,6 +113,8 @@ const hackathons =
     "title": "DubHacks",
     "university": "University of Washington",
     "city": "Seattle",
+    "state": "WA",
+    "country": "USA",
     "coordinates": [47.6553, -122.3035]
   },
   {
@@ -96,6 +122,8 @@ const hackathons =
     "title": "HackDuke",
     "university": "Duke University",
     "city": "Durham",
+    "state": "NC",
+    "country": "USA",
     "coordinates": [36.0014, -78.9382]
   },
   {
@@ -103,6 +131,8 @@ const hackathons =
     "title": "VTHacks",
     "university": "Virginia Tech",
     "city": "Blacksburg",
+    "state": "VA",
+    "country": "USA",
     "coordinates": [37.2285, -80.4178]
   },
   {
@@ -110,6 +140,8 @@ const hackathons =
     "title": "HackRice",
     "university": "Rice University",
     "city": "Houston",
+    "state": "TX",
+    "country": "USA",
     "coordinates": [29.7174, -95.4018]
   },
   {
@@ -117,6 +149,8 @@ const hackathons =
     "title": "BigRed//Hacks",
     "university": "Cornell University",
     "city": "Ithaca",
+    "state": "NY",
+    "country": "USA",
     "coordinates": [42.444, -76.5019]
   },
   {
@@ -124,6 +158,8 @@ const hackathons =
     "title": "TartanHacks",
     "university": "Carnegie Mellon University",
     "city": "Pittsburgh",
+    "state": "PA",
+    "country": "USA",
     "coordinates": [40.4432, -79.9428]
   },
   {
@@ -131,6 +167,8 @@ const hackathons =
     "title": "Hack@Brown",
     "university": "Brown University",
     "city": "Providence",
+    "state": "RI",
+    "country": "USA",
     "coordinates": [41.8268, -71.4025]
   },
   {
@@ -138,6 +176,8 @@ const hackathons =
     "title": "TAMUhack",
     "university": "Texas A&M University",
     "city": "College Station",
+    "state": "TX",
+    "country": "USA",
     "coordinates": [30.628, -96.3344]
   },
   {
@@ -145,6 +185,8 @@ const hackathons =
     "title": "SwampHacks",
     "university": "University of Florida",
     "city": "Gainesville",
+    "state": "FL",
+    "country": "USA",
     "coordinates": [29.6436, -82.3549]
   },
   {
@@ -152,6 +194,8 @@ const hackathons =
     "title": "HackUMass",
     "university": "University of Massachusetts Amherst",
     "city": "Amherst",
+    "state": "MA",
+    "country": "USA",
     "coordinates": [42.3868, -72.5301]
   },
   {
@@ -159,6 +203,8 @@ const hackathons =
     "title": "UGAHacks",
     "university": "University of Georgia",
     "city": "Athens",
+    "state": "GA",
+    "country": "USA",
     "coordinates": [33.948, -83.3773]
   },
   {
@@ -166,13 +212,17 @@ const hackathons =
     "title": "SD Hacks",
     "university": "UC San Diego",
     "city": "La Jolla",
+    "state": "CA",
+    "country": "USA",
     "coordinates": [32.8801, -117.234]
   },
   {
     "id": 25,
     "title": "Hoya Hacks",
     "university": "Georgetown University",
-    "city": "Washington D.C.",
+    "city": "Washington",
+    "state": "DC",
+    "country": "USA",
     "coordinates": [38.9076, -77.0723]
   },
   {
@@ -180,6 +230,8 @@ const hackathons =
     "title": "HopHacks",
     "university": "Johns Hopkins University",
     "city": "Baltimore",
+    "state": "MD",
+    "country": "USA",
     "coordinates": [39.3299, -76.6205]
   },
   {
@@ -187,6 +239,8 @@ const hackathons =
     "title": "AthenaHacks",
     "university": "University of Southern California",
     "city": "Los Angeles",
+    "state": "CA",
+    "country": "USA",
     "coordinates": [34.0224, -118.2851]
   },
   {
@@ -194,6 +248,8 @@ const hackathons =
     "title": "RoseHack",
     "university": "UC Riverside",
     "city": "Riverside",
+    "state": "CA",
+    "country": "USA",
     "coordinates": [33.9737, -117.3281]
   },
   {
@@ -201,6 +257,8 @@ const hackathons =
     "title": "DevFest",
     "university": "Columbia University",
     "city": "New York City",
+    "state": "NY",
+    "country": "USA",
     "coordinates": [40.8075, -73.9626]
   },
   {
@@ -208,6 +266,8 @@ const hackathons =
     "title": "SpartaHack",
     "university": "Michigan State University",
     "city": "East Lansing",
+    "state": "MI",
+    "country": "USA",
     "coordinates": [42.7018, -84.4822]
   }
 ];
