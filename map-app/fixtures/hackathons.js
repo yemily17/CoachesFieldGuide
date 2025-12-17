f4c0fffe5b512a70137cab47d9c20444af2c1496
Lavanya Sharma <Larazinbox@gmail.com>
2025-12-16 23:47:35 -0800

    update hackathons.js file with university tags

--- map-app/fixtures/hackathons.js
+++ map-app/fixtures/hackathons.js
@@ -1,39 +1,214 @@
-const hackathons = [
+const hackathons = 
+[
   {
-    id: 1,
-    title: 'Hack The North',
-    city: 'Waterloo',
-    coordinates: [43.4674, -80.5204],
+    "id": 1,
+    "title": "Hack The North",
+    "university": "University of Waterloo",
+    "city": "Waterloo",
+    "coordinates": [43.4723, -80.5449]
   },
   {
-    id: 2,
-    title: 'TreeHacks',
-    city: 'Stanford',
-    coordinates: [37.4246, -122.1661],
+    "id": 2,
+    "title": "TreeHacks",
+    "university": "Stanford University",
+    "city": "Stanford",
+    "coordinates": [37.4275, -122.1697]
   },
   {
-    id: 3,
-    title: 'MHacks',
-    city: 'Ann Arbor',
-    coordinates: [42.2780, -83.7382],
+    "id": 3,
+    "title": "MHacks",
+    "university": "University of Michigan",
+    "city": "Ann Arbor",
+    "coordinates": [42.278, -83.7382]
   },
   {
-    id: 4,
-    title: 'CalHacks',
-    city: 'Berkeley',
-    coordinates: [37.8715, -122.2730],
+    "id": 4,
+    "title": "Cal Hacks",
+    "university": "UC Berkeley",
+    "city": "Berkeley",
+    "coordinates": [37.8715, -122.273]
   },
   {
-    id: 5,
-    title: 'AthenaHacks',
-    city: 'Los Angeles',
-    coordinates: [34.0223563,-118.28998793,4.0223563,-118.2899879],
+    "id": 5,
+    "title": "PennApps",
+    "university": "University of Pennsylvania",
+    "city": "Philadelphia",
+    "coordinates": [39.9522, -75.1932]
   },
   {
-    id: 6,
-    title: 'DevFest',
-    city: "New York City",
-    coordinates: [40.8075395, -73.96514764,0.8075395, -73.9651476],
+    "id": 6,
+    "title": "HackHarvard",
+    "university": "Harvard University",
+    "city": "Cambridge",
+    "coordinates": [42.377, -71.1167]
+  },
+  {
+    "id": 7,
+    "title": "HackGT",
+    "university": "Georgia Institute of Technology",
+    "city": "Atlanta",
+    "coordinates": [33.7756, -84.3963]
+  },
+  {
+    "id": 8,
+    "title": "LA Hacks",
+    "university": "UCLA",
+    "city": "Los Angeles",
+    "coordinates": [34.0689, -118.4452]
+  },
+  {
+    "id": 9,
+    "title": "HackIllinois",
+    "university": "University of Illinois Urbana-Champaign",
+    "city": "Urbana-Champaign",
+    "coordinates": [40.1106, -88.2073]
+  },
+  {
+    "id": 10,
+    "title": "HackTX",
+    "university": "University of Texas at Austin",
+    "city": "Austin",
+    "coordinates": [30.2849, -97.7341]
+  },
+  {
+    "id": 11,
+    "title": "Boilermake",
+    "university": "Purdue University",
+    "city": "West Lafayette",
+    "coordinates": [40.4237, -86.9212]
+  },
+  {
+    "id": 12,
+    "title": "HackPrinceton",
+    "university": "Princeton University",
+    "city": "Princeton",
+    "coordinates": [40.3431, -74.6551]
+  },
+  {
+    "id": 13,
+    "title": "DubHacks",
+    "university": "University of Washington",
+    "city": "Seattle",
+    "coordinates": [47.6553, -122.3035]
+  },
+  {
+    "id": 14,
+    "title": "HackDuke",
+    "university": "Duke University",
+    "city": "Durham",
+    "coordinates": [36.0014, -78.9382]
+  },
+  {
+    "id": 15,
+    "title": "VTHacks",
+    "university": "Virginia Tech",
+    "city": "Blacksburg",
+    "coordinates": [37.2285, -80.4178]
+  },
+  {
+    "id": 16,
+    "title": "HackRice",
+    "university": "Rice University",
+    "city": "Houston",
+    "coordinates": [29.7174, -95.4018]
+  },
+  {
+    "id": 17,
+    "title": "BigRed//Hacks",
+    "university": "Cornell University",
+    "city": "Ithaca",
+    "coordinates": [42.444, -76.5019]
+  },
+  {
+    "id": 18,
+    "title": "TartanHacks",
+    "university": "Carnegie Mellon University",
+    "city": "Pittsburgh",
+    "coordinates": [40.4432, -79.9428]
+  },
+  {
+    "id": 19,
+    "title": "Hack@Brown",
+    "university": "Brown University",
+    "city": "Providence",
+    "coordinates": [41.8268, -71.4025]
+  },
+  {
+    "id": 20,
+    "title": "TAMUhack",
+    "university": "Texas A&M University",
+    "city": "College Station",
+    "coordinates": [30.628, -96.3344]
+  },
+  {
+    "id": 21,
+    "title": "SwampHacks",
+    "university": "University of Florida",
+    "city": "Gainesville",
+    "coordinates": [29.6436, -82.3549]
+  },
+  {
+    "id": 22,
+    "title": "HackUMass",
+    "university": "University of Massachusetts Amherst",
+    "city": "Amherst",
+    "coordinates": [42.3868, -72.5301]
+  },
+  {
+    "id": 23,
+    "title": "UGAHacks",
+    "university": "University of Georgia",
+    "city": "Athens",
+    "coordinates": [33.948, -83.3773]
+  },
+  {
+    "id": 24,
+    "title": "SD Hacks",
+    "university": "UC San Diego",
+    "city": "La Jolla",
+    "coordinates": [32.8801, -117.234]
+  },
+  {
+    "id": 25,
+    "title": "Hoya Hacks",
+    "university": "Georgetown University",
+    "city": "Washington D.C.",
+    "coordinates": [38.9076, -77.0723]
+  },
+  {
+    "id": 26,
+    "title": "HopHacks",
+    "university": "Johns Hopkins University",
+    "city": "Baltimore",
+    "coordinates": [39.3299, -76.6205]
+  },
+  {
+    "id": 27,
+    "title": "AthenaHacks",
+    "university": "University of Southern California",
+    "city": "Los Angeles",
+    "coordinates": [34.0224, -118.2851]
+  },
+  {
+    "id": 28,
+    "title": "RoseHack",
+    "university": "UC Riverside",
+    "city": "Riverside",
+    "coordinates": [33.9737, -117.3281]
+  },
+  {
+    "id": 29,
+    "title": "DevFest",
+    "university": "Columbia University",
+    "city": "New York City",
+    "coordinates": [40.8075, -73.9626]
+  },
+  {
+    "id": 30,
+    "title": "SpartaHack",
+    "university": "Michigan State University",
+    "city": "East Lansing",
+    "coordinates": [42.7018, -84.4822]
   }
 ];
 
